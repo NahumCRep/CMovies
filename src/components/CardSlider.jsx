74dae697fd6f0eeb4e356aa00fe86b1186d4cfb8
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-22 22:31:44 -0500

    refactor(Root): style of the slider and functionality

--- src/components/CardSlider.jsx
+++ src/components/CardSlider.jsx
@@ -1,24 +1,32 @@
 import React, {useRef} from 'react'
 import style from '../css/cardslider.module.css'
+import {MdArrowForwardIos, MdArrowBackIos} from 'react-icons/md'
 
 const CardSlider = ({ children }) => {
   const cardSliderRef = useRef(null)
-  // const scrollSlider = () => {
-  //   cardSliderRef.scrollLeft -= 200
-  //   console.log(cardSliderRef.current.scrollLeft)
-  // }
-  const scrollSlider = (scrollValue) => {
-    cardSliderRef.current.scrollLeft += scrollValue
-    console.log(cardSliderRef.current.scrollLeft)
+
+  const scrollToLeft = () => {
+    cardSliderRef.current.scrollLeft -= (cardSliderRef.current.children[0].offsetWidth + 10)
+    // cardSliderRef.current.scrollLeft += scrollValue
   }
+
+  const scrollToRight = () => {
+    cardSliderRef.current.scrollLeft += (cardSliderRef.current.children[0].offsetWidth + 10)
+    // cardSliderRef.current.scrollLeft += scrollValue
+  }
+
 
   return (
     <div  className={style.cardlsider}>
       <div ref={cardSliderRef} className={style.cardslider__scrollable}>
           {children}
       </div>
-      <button onClick={() => scrollSlider(-200)} className={style.cardlsider__leftbtn}>left</button>
-      <button onClick={() => scrollSlider(200)} className={style.cardlsider__rightbtn}>right</button> 
+      <button onClick={() => scrollToLeft()} className={style.cardlsider__leftbtn}>
+          <MdArrowBackIos size={20} />
+      </button>
+      <button onClick={() => scrollToRight()} className={style.cardlsider__rightbtn}>
+          <MdArrowForwardIos size={20} />  
+      </button> 
     </div>
   )
 }
