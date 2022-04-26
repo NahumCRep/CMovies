569de2e6931c70ac4ee3743c5b253a72a28f3023
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-25 23:01:46 -0500

    refactor(Root): name as a link

--- src/components/cards/PopularCard.jsx
+++ src/components/cards/PopularCard.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import style from '../../css/popularcard.module.css'
 import { backdropURL } from '../../config'
-
+import { Link } from 'react-router-dom'
 
 const PopularCard = ({itemData, itemType}) => {
   return (
     <article className={style.popularcard}>
         <img src={`${backdropURL}${itemData.backdrop_path}`} className={style.popularcard__img} /> 
         <div className={style.popularcard__title}>
-            <p>{itemType == 'movie' ? itemData.title : itemData.name}</p>
+            <Link to={itemType == 'movie' ? `/movie/${itemData.id}` : `/tvshow/${itemData.id}`}>
+              {itemType == 'movie' ? itemData.title : itemData.name}
+            </Link>
         </div>
     </article>
   )
