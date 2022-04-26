f8f33bb26760dacf26854370df066b70bb49434a
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-25 22:59:48 -0500

    feat(Root): scroll to top on route change

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import React from 'react'
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Home, Movies, TvShows, MovieDetails, TvShowDetails, Login } from './pages'
 
 function App() {
   return (
     <BrowserRouter>
-      <Navbar />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/movies' element={<Movies />} />
-        <Route path='/tvshows' element={<TvShows />} />
-        <Route path='/movie/:id' element={<MovieDetails />} />
-        <Route path='/tvshow/:id' element={<TvShowDetails />} />
-        <Route path='/login' element={<Login />} />
-      </Routes>
-      <Footer />
+      <ScrollToTop>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/movies' element={<Movies />} />
+          <Route path='/tvshows' element={<TvShows />} />
+          <Route path='/movie/:id' element={<MovieDetails />} />
+          <Route path='/tvshow/:id' element={<TvShowDetails />} />
+          <Route path='/login' element={<Login />} />
+        </Routes>
+        <Footer />
+      </ScrollToTop>
     </BrowserRouter>
   );
 }
