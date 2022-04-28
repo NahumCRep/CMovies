51accc9f5c5b10b762048c7d3ccf97238cae0b01
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-28 18:59:34 -0500

    feat(Root): similar movies or tv shows

---
+++ src/components/details/Similars.jsx
@@ -0,0 +1,39 @@
+import React, {useState, useEffect} from 'react'
+import style from '../../css/similars.module.css'
+import SimpleCard from '../cards/SimpleCard'
+import Spinner from '../Spinner'
+import { getListLimit } from '../../api'
+
+const Similars = ({showID, type, itemsLimit}) => {
+  const [similarList, setSimilarList] = useState([])
+
+  useEffect(()=>{
+    getListLimit(`/${type == 'movie' ? 'movie': 'tv' }/${showID}/similar`, itemsLimit)
+    .then(res => {
+        console.log('similars', res)
+        setSimilarList(res)
+    })
+    .catch(error => console.log(error))
+  },[])
+
+  return (
+    <div className={style.similar__container}>
+      <h1 className={style.similar__title}>
+        { type == 'movie' ? 'Similar Movies' : 'Similar TV Shows'}
+      </h1>
+      {
+        similarList 
+          ? (
+            similarList.map((similar)=>{
+                return(
+                  <SimpleCard cardType={type} cardData={similar} />
+                )
+            })
+          )
+          : <Spinner />
+      }
+    </div>
+  )
+}
+
+export default Similars
