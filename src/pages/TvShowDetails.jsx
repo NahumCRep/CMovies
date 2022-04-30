6a8f5d85e39b6ca43ff7750a72bd261413da99f9
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-30 16:52:31 -0500

    fix(Root): add id param in the useEffect dependencies

--- src/pages/TvShowDetails.jsx
+++ src/pages/TvShowDetails.jsx
@@ -1,17 +1,36 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
+import style from '../css/details.module.css'
+import DetailsPage from '../components/layouts/DetailsPage'
+import YoutubeEmbed from '../components/details/YoutubeEmbed'
 import { useParams } from 'react-router-dom'
 import { get } from '../api'
 
 const TvShowDetails = () => {
-  const {id} = useParams()
-  useEffect(()=>{
-    
+  const [tvshow, setTvShow] = useState([])
+  const { id } = useParams()
+
+  useEffect(() => {
     get(`/tv/${id}`)
-      .then(res => console.log(res))
+      .then(res => {
+        console.log(res)
+        setTvShow(res.data)
+      })
       .catch(error => console.log(error))
-  },[])
+  }, [id])
+
   return (
-    <div>TvShowDetails</div>
+    <section className={style.detail__section}>
+      {
+        tvshow && (
+          <>
+            <DetailsPage show={tvshow} showID={id} showType={"tv"} >
+             
+            </DetailsPage>
+          </>
+        )
+      }
+
+    </section>
   )
 }
 
