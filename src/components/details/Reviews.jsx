b8cbef05ad7811030cf11a1d09a5dc5de3ab7d02
Nahum Casco <nahumcasco.v@gmail.com>
2022-04-30 16:51:48 -0500

    feat(Root): add loading state and style to empty message

--- src/components/details/Reviews.jsx
+++ src/components/details/Reviews.jsx
@@ -1,17 +1,35 @@
 import React, { useState, useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import style from '../../css/reviews.module.css'
-import {FaPlus} from 'react-icons/fa'
+import { FaPlus } from 'react-icons/fa'
 import { get } from '../../api'
 import ReviewBox from './ReviewBox'
+import Spinner from '../Spinner'
 
-const Reviews = ({ movieID }) => {
+const Reviews = ({ showID, showType }) => {
     const [allReviews, setAllReviews] = useState([])
+    const [showReviews, setShowReviews] = useState(false)
+    const [isLoading, setIsLoading] = useState(false)
+    const [isEmpty, setIsEmpty] = useState(false)
+    const location = useLocation()
+
+    useEffect(() => {
+        setAllReviews([])
+        setIsEmpty(false)
+        setShowReviews(false)
+        setIsLoading(false)
+    },[location])
 
     const getReviews = () => {
-        get(`/movie/${movieID}/reviews`)
+        setShowReviews(true)
+        setIsLoading(true)
+        let queryType = showType == 'movie' ? 'movie' : 'tv'
+        get(`/${queryType}/${showID}/reviews`)
             .then(res => {
                 console.log('revs', res)
+                if(res.data.results.length == 0) setIsEmpty(true)
                 setAllReviews(res.data.results)
+                setIsLoading(false)
             })
             .catch(error => console.error(error))
     }
@@ -21,12 +39,20 @@ const Reviews = ({ movieID }) => {
             <button onClick={() => getReviews()} className={style.reviews__showbtn}><FaPlus /> show reviews</button>
             <div className={style.reviews__list}>
                 {
-                    allReviews.length > 0 && (
-                        allReviews.map(review => {
-                            return(
-                                <ReviewBox key={review.id} review={review} />
-                            )  
-                        })
+                    showReviews && (
+                        isLoading
+                        ? <Spinner />
+                        : (
+                            !isEmpty
+                            ? (
+                                allReviews.map(review => {
+                                    return (
+                                        <ReviewBox key={review.id} review={review} />
+                                    )
+                                })
+                            )
+                            : <p className={style.reviews__list_message}>no reviews</p>
+                        )
                     )
                 }
             </div>
