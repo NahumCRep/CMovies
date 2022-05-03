8955f8c8d1a54de56c247d6c132cb756e6e8fbdb
Nahum Casco <nahumcasco.v@gmail.com>
2022-05-02 22:36:41 -0500

    refactor(Root): aside section name change

---
+++ src/components/sections/DayTrending.jsx
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from 'react'
+import style from '../../css/daytrending.module.css'
+import { getListLimit } from '../../api'
+import Spinner from '../Spinner'
+import SimpleCard from '../cards/SimpleCard'
+
+const DayTrending = ({ type, itemsLimit }) => {
+  const [trendingList, setTrendingList] = useState([])
+
+  useEffect(() => {
+    getListLimit(`/trending/${type == 'movie' ? 'movie' : 'tv'}/week`, itemsLimit)
+      .then(res => {
+        setTrendingList(res)
+      })
+      .catch(error => console.error(error))
+  }, [])
+
+  return (
+    <section className={style.aside__section}>
+      <h3 className={style.aside__title}>{type == "movie" ? 'Movies' : 'Tv Shows'}</h3>
+      <div className={style.aside__section_cards}>
+        {
+          trendingList
+            ? (
+              trendingList.map((trending) => {
+                return (
+                  <SimpleCard key={trending.id} cardType={type} cardData={trending} />
+                )
+              })
+            )
+            : <Spinner />
+        }
+      </div>
+    </section>
+  )
+}
+
+export default DayTrending
