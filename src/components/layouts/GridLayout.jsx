af1a1789638ced172e5e8115812c43251f826b7f
Nahum Casco <nahumcasco.v@gmail.com>
2022-05-07 22:43:55 -0500

    fix(Root): card type prop added

--- src/components/layouts/GridLayout.jsx
+++ src/components/layouts/GridLayout.jsx
@@ -2,13 +2,13 @@ import React from 'react'
 import style from '../../css/layouts/grid_layout.module.css'
 import Card from '../cards/Card'
 
-const GridLayout = ({dataList}) => {
+const GridLayout = ({dataList, listType}) => {
   return (
     <section className={style.GridLayout__container}>
         {
             dataList.map((item)=>{
                 return(
-                    <Card key={item.id} cardData={item} />
+                    <Card key={item.id} cardData={item} cardType={listType} />
                 )
             })
         }
