6643d9226173e2eb6f20f335a27eeece5b72b4fb
Nahum Casco <nahumcasco.v@gmail.com>
2022-05-08 22:00:52 -0500

    feat(Root): functional category component

--- src/components/sections/Categories.jsx
+++ src/components/sections/Categories.jsx
@@ -1,21 +1,36 @@
 import React, { useState, useEffect } from 'react'
 import style from '../../css/sections/categories.module.css'
+import { useNavigate } from 'react-router-dom'
 import { get } from '../../api'
 import { motion, AnimatePresence } from 'framer-motion'
+import { FaAngleDown, FaPlus } from 'react-icons/fa'
 
 const Categories = ({ categoryType }) => {
   const [categories, setCategories] = useState([])
   const [openModal, setOpenModal] = useState(false)
+  const navigate = useNavigate()
 
   useEffect(() => {
     get(`/genre/${categoryType}/list`)
-      .then(res => console.log('categories', res))
+      .then(res => {
+        console.log('categories', res)
+        setCategories(res.data.genres)
+      })
       .catch(error => console.error(error))
   }, [])
 
+  const handleLink = (categoryID) => {
+    setOpenModal(false)
+    categoryType == 'movie' 
+      ? navigate(`/movies?g=${categoryID}`)
+      : navigate(`/tvshows?g=${categoryID}`)
+  }
+
   return (
     <div className={style.category__container}>
-      <button className={style.category__btn} onClick={() => setOpenModal(!openModal)}>Categories</button>
+      <button className={style.category__btn} onClick={() => setOpenModal(!openModal)}>
+          { openModal ?  'Close Categories':'Categories'}
+      </button>
       <AnimatePresence>
         {
           openModal && (
@@ -24,7 +39,13 @@ const Categories = ({ categoryType }) => {
               exit={{ scale: 0 }}
               className={style.category__list_container}
             >
-
+              {
+                categories && (
+                  categories.map((category) => {
+                    return <button key={category.id} className={style.category__link} onClick={() => handleLink(category.id)}>{category.name}</button>
+                  })
+                )
+              }
             </motion.div>
           )
         }
